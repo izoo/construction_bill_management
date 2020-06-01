59b1089a8e14b41d4fef5cc0f57cbae96d1d4a56
izoo <aisacsmooth@gmail.com>
2020-06-01 03:34:08 -0400

    updated migrations

---
+++ database/migrations/2020_04_10_175338_create_sites_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sit_name');
+            $table->integer('phone_number')->nullable();
+            $table->string('email')->nullable();
+            $table->string('estimated_start_date')->nullable();
+            $table->string('estimated_end_date')->nullable();
+            $table->string('location')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}
